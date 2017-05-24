c87b9d4a482c716ee02c643b28c3c374cafb6137
rvdpas <rvdpas94@gmail.com>
2017-05-24 12:22:26 +0200

    load tweets above instead of below the last one

--- final/public/js/main.js
+++ final/public/js/main.js
@@ -4,20 +4,10 @@ var tweetsHolder = document.querySelector('#tweetsHolder');
 
 socket.on('new tweet', newTweet);
 
+// Create tweet shell and print it to the page
 function newTweet(tweet) {
-  var li = document.createElement('li');
-  li.className = "message";
-  li.innerHTML = '<strong>' + tweet.user.screen_name + '</strong>: ' + tweet.text;
-
-  tweetsHolder.appendChild(li);
+  tweetsHolder.insertAdjacentHTML('afterbegin', '<li><strong>' + tweet.user.screen_name + '</strong>: ' + tweet.text + '</li>');
 }
-
-socket.on('server-emit', function (data) {
-  var msg = document.getElementById('msg');
-  msg = data.current;
-  msg.replace = data.current;
-  socket.emit('client-emit', {});
-});
 
 socket.on('update counter', function(counter) {
   var counterElement = document.querySelector('.counter')
